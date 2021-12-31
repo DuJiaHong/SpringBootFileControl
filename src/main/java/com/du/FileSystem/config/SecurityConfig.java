2ea94d827f615b345bd08c640508a48dcc5692d8
du <dujiahong@four-faith.com>
2021-12-31 18:35:06 +0800

    add springboot security

--- src/main/java/com/du/FileSystem/config/SecurityConfig.java
+++ src/main/java/com/du/FileSystem/config/SecurityConfig.java
@@ -1,25 +1,28 @@
-package com.du.FileSystem.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-@Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-//    注入passwordEncoder类到spring容器中
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String passwd = passwordEncoder.encode("test");
-        auth.inMemoryAuthentication().withUser("test").password(passwd).roles("admin");
-    }
-}
+//package com.du.FileSystem.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+///**
+// * 在代码中实现配置固定用户名和密码
+// */
+//@Configuration
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+////    注入passwordEncoder类到spring容器中
+//    @Bean
+//    public PasswordEncoder passwordEncoder(){
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+//        String passwd = passwordEncoder.encode("test");
+//        auth.inMemoryAuthentication().withUser("test").password(passwd).roles("admin");
+//    }
+//}
---
+++ src/main/java/com/du/FileSystem/config/SecurityConfigNew.java
@@ -0,0 +1,29 @@
+package com.du.FileSystem.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfigNew extends WebSecurityConfigurerAdapter {
+
+//    注入passwordEncoder类到spring容器中
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+
+    }
+}
